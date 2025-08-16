71d485b37efb9c35e7b1d0a9a1f89d34a0722173
raihanelsar <raihan.elsar21@gmail.com>
2025-08-16 12:57:16 +0700

    Update contact.php

--- content/contact.php
+++ content/contact.php
@@ -73,18 +73,3 @@ $data = mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT * FROM contact LIMIT 1
 
   </div>
 </section>
-<h2>Contact</h2>
-
-<?php if ($data): ?>
-    <p><strong>Address:</strong><br><?= htmlspecialchars($data['address']) ?></p>
-    <p><strong>Phone:</strong><br><?= htmlspecialchars($data['phone']) ?></p>
-    <p><strong>Email:</strong><br><?= htmlspecialchars($data['email']) ?></p>
-
-    <?php if (!empty($data['map_embed'])): ?>
-        <div class="map-embed" style="margin-top:15px;">
-            <?= $data['map_embed'] ?>
-        </div>
-    <?php endif; ?>
-<?php else: ?>
-    <p>No contact data available.</p>
-<?php endif; ?>
